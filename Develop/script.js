d56fc9cfb786b7f2d9534698ca41f3c193fe08a1
Navneet Kaur <nbirdi@yahoo.com>
2022-05-28 17:16:59 +0800

    few errors fixed in Javascript file

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,7 @@
 
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
-//console.log(generateBtn);
-function askPasswordLength(){
-// series of prompts for password
 
-// first password length
-const passwordLength = prompt("Select a password length between 8 and 128");
-
-//if password length correct
-if (passwordLength <=8 && passwordLength <= 128){
-  console.log(passwordLength);
-}else{
-  console.log("Please choose password length between 8 and 128 \n Thank You");
-}
 
 //character types to include in password
 // 1. creating arrays for character types
@@ -22,7 +10,27 @@ const uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', '
 const lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const specialCharacters = ['@','%','+','\\',"'",'!','#','$','^','?',':',',', ')', '(', '}', '{', ']', '[', '~','-', '_','.'];
 
+
+//console.log(generateBtn);
+function askPasswordLength(){
+
+
+// series of prompts for password
+// first password length
+const passwordLength = Number(prompt("Select a password length between 8 and 128"));
+
+
+//can only accept numeric value
+
+//if password length correct
+if (passwordLength <=8 || passwordLength >= 128 || isNaN(passwordLength)){ 
+  console.log(passwordLength);
+  askPasswordLength();
 }
+return passwordLength;
+}
+
+
 //asking questions
 function askCriteria(){
 //character types 
@@ -31,54 +39,53 @@ const upperC = confirm("Do you want to use upper case characters?");
 const numberC = confirm("Do you want to use numeric characters?");
 const specialC = confirm("Do yu want to use special characters?");
  
-// check if prompts have been selected for password
 
+// check if prompts have been selected for password
 if  (lowerC == true|| upperC == true|| numberC == true|| specialC == true){
   return{
     lowerC,upperC,numberC,specialC,
   };
 }
 
+
 // if user didn't do that, re-ask the criteria questions
 return askCriteria();
-
 } 
 
-//when user click on button
 
+//when user click on button
 generateBtn.addEventListener('click',function(event){
  
   const passwordLength = askPasswordLength();
   const criteria = askCriteria();
 
 
-  //generating a password and building a character set
+//generating a password and building a character set
+ let selectedCharacters = "";
 
-  let selectedCharacters = []; //empty array to print selected characters as a password
-
-  if (lowerC == true){
-    selectedCharacters = selectedCharacters + lowercaseCharacters;
+  if (criteria.lowerC === true){
+    selectedCharacters.push(...lowercaseCharacters);
       }
 
- if (upperC == true){
-selectedCharacters = selectedCharacters + uppercaseCharacters;
+ if (criteria.upperC === true){
+    selectedCharacters.push(...uppercaseCharacters);
           }
 
- if (numberC == true){
-    selectedCharacters = selectedCharacters + numericCharacters;
+ if (criteria.numberC === true){
+    selectedCharacters.push(...numericCharacters);
           }
 
-  if (specialC == true){
-    selectedCharacters = selectedCharacters + specialCharacters;
+  if (criteria.specialC === true){
+    selectedCharacters .push(...specialCharacters);
           }
             
-
 
 // password 
 
 let password = "";
 for  (let index = 0; index < passwordLength; index++){
   const randomCharacter = selectedCharacters[ Math.floor(Math.random() * selectedCharacters.length) ]
+  console.log(randomCharacter);
   password += randomCharacter;
 }
  document.getElementById('password').value = password;
